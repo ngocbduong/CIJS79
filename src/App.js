4bdb7b167b187cd5da44ca8fc79f995be60c1069
ngocbduong <baongoc18592@gmail.com>
2022-12-24 23:45:21 +0700

    UP BTVN3

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import HomeWork from "./Components/BTVN2/Header";
 import HomeWorkBody from "./Components/BTVN2/Body";
 import HomeWorkF from "./Components/BTVN2/Footer";
-
-function SplitFrame(props) {
-  return (
-    <div>
-      <div>
-      {props.header}
-      </div>
-      <div>
-        {props.body}
-      </div>
-      <div>
-        {props.footer}
-      </div>
-    </div>
-  );
-}
+import App2 from "./Components/BTVN3";
 
 function App() {
   return (
-    <SplitFrame
-    header={<HomeWork/>}
-    body={<HomeWorkBody/>}
-    footer={<HomeWorkF/>}
-    />
-  )
+    <div>
+      <h2>BTVN số 1: </h2>
+      <HomeWork />
+      <HomeWorkBody />
+      <HomeWorkF />
+      <h2>BTVN số 2: </h2>
+      <App2/>
+    </div>
+  );
 }
 
 export default App;
